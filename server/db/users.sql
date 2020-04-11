f0b2a1b055b3b872cbc0b1843e8cb75aca3139e9
Nhat Nguyen <natt.lefevre@gmail.com>
2020-04-10 21:16:57 -0700

    ⚡ Table schema and connection

--- server/db/users.sql
+++ server/db/users.sql
@@ -1,19 +1,28 @@
 CREATE TABLE IF NOT EXISTS users (
-  id            char(32) PRIMARY KEY,
+  id            char(36) PRIMARY KEY,
   first_name    varchar(255) NOT NULL,
   last_name     varchar(255) NOT NULL,
   username      varchar(255) UNIQUE NOT NULL,
   email         varchar(255) UNIQUE NOT NULL,
   password      varchar(255) NOT NULL,
-  time_created  timestampz NOT NULL,
-  time_actived  timestampz NOT NULL
+  refresh_token text NOT NULL,
+  time_created  timestamptz DEFAULT now(),
+  time_actived  timestamptz DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS users_dev (
-  id            char(32) PRIMARY KEY,
-  first_name    varchar(255) NOT NULL,
-  last_name     varchar(255) NOT NULL,
-  username      varchar(255) UNIQUE NOT NULL,
-  email         varchar(255) UNIQUE NOT NULL,
-  password      varchar(255) NOT NULL,
+INSERT INTO users VALUES (
+  '00000000-0000-0000-0000-000000000000',
+  'James',
+  'Bond',
+  'jamesbond',
+  'jamesbond@gmail.com',
+  '$2b$10$giaS5kXc/.BCtC/KdRzvW..B8zA/V3djEc4eeDyWYjAd5UWXs9FbW',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ0ZDMzNjNmLWRjNmItNDAyMS1hNWYxLTMwNzY1ZjFkNGZlNCIsImlhdCI6MTU4NjU3NTYzMn0.LdOJV4bEPTmcOgFOGmK5BMsFrbSxIwuMnA13039vYf4'
 );
+
+-- \x expanded display mode
+SELECT * FROM users;
+
+SELECT * FROM users1;
+
+DROP TABLE users;
--- server/js/connect.js
+++ server/js/connect.js
@@ -1,8 +1,42 @@
+const uuid = require('uuid');
+const faker = require('faker');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'kbank',
   password: 'password',
   port: 5432,
-})
+});
+
+const id = uuid.v4();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const username = faker.internet.userName();
+const email = faker.internet.email();
+const password = faker.internet.password();
+const hashPassword = bcrypt.hashSync(password, 10);
+const refreshToken = jwt.sign({ id }, 'secret');
+
+const query = {
+  text: 'INSERT INTO users VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+  values: [id, firstName, lastName, username, email, hashPassword, refreshToken],
+};
+
+const main = async () => {
+  try {
+    // Add user to database
+    await pool.query(query);
+
+    // Gets the list of users
+    const users = await pool.query('SELECT * FROM users');
+    console.table(users.rows);
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+main();
